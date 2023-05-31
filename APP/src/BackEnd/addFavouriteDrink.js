cd93c5ea0575751f98c52c233a138b7a013d322b
Samessprog <100785560+Samessprog@users.noreply.github.com>
2023-05-31 19:18:04 +0200

    Add to favorites 1.0

--- APP/src/BackEnd/addFavouriteDrink.js
+++ APP/src/BackEnd/addFavouriteDrink.js
@@ -1,39 +1,27 @@
-// const express = require('express');
-// const router = express.Router();
-// const db = require('./DB');
+const express = require('express');
+const router = express.Router();
+const db = require('./DB')
 
-// router.post('/', async (req, res) => {
-//   const { id, sessionidx } = req.body;
 
-//   if (!sessionidx) {
-//     return res.status(400).json({ success: false, message: 'Missing sessionidx' });
-//   }
+router.post('/', async (req, res) => {
+    const { id, sessionidx } = req.body;
 
-//   try {
-//     const querySelect = `SELECT ID_FavouriteDrink FROM users WHERE email = '${sessionidx}'`;
-//     const result = await db.query(querySelect);
+    if(sessionidx === null || sessionidx === undefined) {
+        return
+    }
 
-//     let currentFavouriteDrinks = [];
-//     if (result.length > 0) {
-//       currentFavouriteDrinks = JSON.parse(result[0].ID_FavouriteDrink);
-//     }
 
-//     currentFavouriteDrinks.push(id);
+    const query = `UPDATE users SET ID_FavouriteDrink = ${id} WHERE email = '${sessionidx}'`;
 
-//     const updatedFavouriteDrinks = JSON.stringify(currentFavouriteDrinks);
+    console.log(query)
 
-//     const queryUpdate = `UPDATE users SET ID_FavouriteDrink = '${updatedFavouriteDrinks}' WHERE email = '${sessionidx}'`;
-//     await db.query(queryUpdate);
+    try {
+        await db.query(query);
+        res.status(200).json({ success: true, message: 'ID added to FavouriteDrink' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, message: 'An error occurred' });
+    }
+});
 
-//     res.status(200).json({
-//       success: true,
-//       message: 'ID added to FavouriteDrink',
-//       updatedFavouriteDrinks: currentFavouriteDrinks,
-//     });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ success: false, message: 'An error occurred' });
-//   }
-// });
-
-// module.exports = router;
+module.exports = router;
