dbb6c370354acca5f7388242351a978049a4a9ec
Samessprog <100785560+Samessprog@users.noreply.github.com>
2023-07-12 22:03:43 +0200

    AdminStyles

--- APP/src/JS/Profile/admin/Admin.js
+++ APP/src/JS/Profile/admin/Admin.js
@@ -3,10 +3,58 @@ import React from "react";
 
 function Admin() {
 
+
+
+
+    
+
+
     return (
-        <div className="admin-container">
-           
-s
+        <div className="admin-container p-3 p-sm-4 ">
+
+            <div className="admin-header-holder">
+                <header className="fs-2 admin-header">Hello admin NAME</header>
+            </div>
+
+            <div className="users-chart-holder mt-5 d-flex justify-content-center align-items-center ms-sm-1  m-md-3">
+                <div className="">
+                    miejsce na wykres od usera i opcje jego zmiany
+                </div>
+
+            </div>
+
+            <div className="users-AND-drinks-db-holder  ms-sm-1  m-md-3 ">
+                <div className="d-flex justify-content-between ">
+                    <div className="d-flex ms-2 mt-0 optional-buttons-holder mt-2">
+                        <div className="pb-0 me-2">
+                            <button className="optional-buttons">Drinks</button>
+                        </div>
+                        <div className="">
+                            <button className="optional-buttons">Users</button>
+                        </div>
+                    </div>
+
+                    <div className="d-flex mt-3 me-3 ">
+                        <div className="me-4">
+                            <input className="searching-items-admin ps-3 pe-3" type="text" placeholder="enter the name you are looking for"></input>
+                        </div>
+                        <div className="data-filtering-holder">
+                            <svg className="data-filtering-icon" xmlns="http://www.w3.org/2000/svg" height="40" viewBox="0 -960 960 960" width="40"><path d="M440-160q-17 0-28.5-11.5T400-200v-240L163.333-742q-14.333-18-4.166-38 10.166-20 32.833-20h576q22.667 0 32.833 20 10.167 20-4.166 38L560-440v240q0 17-11.5 28.5T520-160h-80Zm40-286.666 226.001-286.668H253.999L480-446.666Zm0 0Z" /></svg>
+                        </div>
+                    </div>
+
+                </div>
+                <div className="d-flex justify-content-center align-items-center">
+                    <div>
+                        BRAK USERÓW
+                    </div>
+
+                    <div>
+                        BRAK DRINKÓW
+                    </div>
+
+                </div>
+            </div>
 
 
 
