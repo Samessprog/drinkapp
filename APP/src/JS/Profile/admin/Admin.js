b54abed6c446e9247d0adac7e59b3eb77ced3689
Samessprog <100785560+Samessprog@users.noreply.github.com>
2023-07-16 21:12:09 +0200

    AdminRes

--- APP/src/JS/Profile/admin/Admin.js
+++ APP/src/JS/Profile/admin/Admin.js
@@ -25,8 +25,8 @@ function Admin() {
             </div>
 
             <div className="users-AND-drinks-db-holder  ms-sm-1  m-md-3 ">
-                <div className="d-flex justify-content-between ">
-                    <div className="d-flex ms-3 mt-0 optional-buttons-holder mt-2">
+                <div className="d-sm-flex justify-content-between col-12 mb-5 d-flex-column align-items-center">
+                    <div className="d-flex ms-3 mt-0 optional-buttons-holder mt-2 col-6 ">
                         <div className="pb-0 me-2">
                             <button className="optional-buttons" onClick={() => { setDrinksFlag(true); setUsersFlag(false) }}>Drinks</button>
                         </div>
@@ -35,9 +35,9 @@ function Admin() {
                         </div>
                     </div>
 
-                    <div className="d-flex mt-3 me-3 ">
-                        <div className="me-4">
-                            <input className="searching-items-admin ps-3 pe-3" type="text" placeholder="enter the name you are looking for"></input>
+                    <div className="d-flex mt-3 me-3  d-flex justify-content-center  justify-content-sm-end ">
+                        <div className="me-4 col-8 col-sm-12">
+                            <input className="searching-items-admin ps-3 pe-3 col-12" type="text" placeholder="enter the name you are looking for"></input>
                         </div>
                         <div className="data-filtering-holder">
                             <svg className="data-filtering-icon" xmlns="http://www.w3.org/2000/svg" height="40" viewBox="0 -960 960 960" width="40"><path d="M440-160q-17 0-28.5-11.5T400-200v-240L163.333-742q-14.333-18-4.166-38 10.166-20 32.833-20h576q22.667 0 32.833 20 10.167 20-4.166 38L560-440v240q0 17-11.5 28.5T520-160h-80Zm40-286.666 226.001-286.668H253.999L480-446.666Zm0 0Z" /></svg>
