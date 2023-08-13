c0c1111c583eeb971cd55909e5349f089b70568b
Samessprog <100785560+Samessprog@users.noreply.github.com>
2023-08-13 19:22:07 +0200

    User DBS respons

--- APP/src/JS/Profile/admin/UsersAdminControlerProfile.js
+++ APP/src/JS/Profile/admin/UsersAdminControlerProfile.js
@@ -13,7 +13,6 @@ function UsersAdminControlerProfile({ elm }) {
     const [newUserEmail, setNewUserEmail] = React.useState('')
     const [newUserPass, setNewUserPass] = React.useState('')
 
-
     useEffect(() => {
         if (elm.userIMG) {
             // Convert the image data to base64
@@ -42,7 +41,7 @@ function UsersAdminControlerProfile({ elm }) {
                 setNewUserEmail('')
                 setNewUserPass('')
                 alert('User Data changes have been made')
-        
+
             } else {
                 setChangingUserDataError(data.message)
             }
@@ -52,32 +51,32 @@ function UsersAdminControlerProfile({ elm }) {
     };
 
     return (
-        <div className="mb-3 ms-3 d-flex  align-items-center drinks-profile-holder  me-3  justify-content-between ">
-            <div className="d-flex  align-items-center">
+        <div className="mb-3 ms-3 d-flex align-items-center drinks-profile-holder  me-3  justify-content-between ">
+            <div className="d-flex align-items-center ">
                 <div className="ms-3 me-4 fs-4">
                     {elm.ID_User}.
                 </div>
-                <div className="justify-content-between d-flex align-items-center position-relative">
-                    <div className="d-flex align-items-center ">
-                        <div className="d-flex align-items-center data-holder">
-                            <div className=" mt-1 mb-1 drink-profile-holder-IMG">
-                                <img className=" drink-profile-img img-fluid " src={userIMGProfileAdmin} alt="loadingErr"></img>
+                <div className="justify-content-between d-flex align-items-center position-relative ">
+                    <div className="d-flex align-items-center flex-xxl-row flex-column ">
+                        <div className="d-flex align-items-center data-holder ">
+                            <div className=" mt-1 mb-1 drink-profile-holder-IMG margin-top-12 ">
+                                <img className=" drink-profile-img img-fluid" src={userIMGProfileAdmin} alt="loadingErr"></img>
                             </div>
-                            <div className="ms-4 d-flex align-items-center">
+                            <div className="ms-4 d-flex  align-items-center margin-top-12">
                                 <label className="fs-5">Nick:</label>
-                                <div className="drink-name-profile ms-2">{elm.Nick}</div>
+                                <div className="drink-name-profile ms-xl-2">{elm.Nick}</div>
                             </div>
 
-                            <div className="ms-4 fs-5">
-                                user Email:
-                                <label className="drink-name-profile ms-2">{elm.email}</label>
+                            <div className="ms-4 fs-5 margin-top-12 ">
+                                <label>ser Email:</label>
+                                <label className="drink-name-profile ms-xl-2">{elm.email}</label>
                             </div>
-                            <div className="ms-4 fs-5">
+                            <div className="ms-4 fs-5 margin-top-12">
                                 User phone
-                                <label className="drink-name-profile ms-2">{elm.phone}</label>
+                                <label className="drink-name-profile ms-xl-2">{elm.phone} 123123123123</label>
                             </div>
                         </div>
-                        <div className="details-button-holder ">
+                        <div className="details-button-holder d-flex mt-xl-3 mb-xl-0 mb-3 flex-column flex-xl-row align-items-center ">
                             <button className="details-button" onClick={() => setUserDataChanger(!userDataChanger)}>
                                 Change User Data
                             </button>
@@ -96,7 +95,6 @@ function UsersAdminControlerProfile({ elm }) {
                                             <svg className="details-change-button" xmlns="http://www.w3.org/2000/svg" height="40" viewBox="0 -960 960 960" width="40"><path d="M379.333-244 154-469.333 201.666-517l177.667 177.667 378.334-378.334L805.333-670l-426 426Z" /></svg>
                                         </button>
 
-
                                     </div>
                                     <label className="userDataChangerError">
                                         {changingUserDataError != null &&
@@ -105,20 +103,21 @@ function UsersAdminControlerProfile({ elm }) {
                                     </label>
                                 </form>
                             }
+                            <div className="d-flex delete-profile ms-4 me-2">
+                                <div className="block-icon-profile me-3">
+                                    <svg lassName="block-icon-profile" xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 -960 960 960" width="48"><path d="M480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-60q142.375 0 241.188-98.812Q820-337.625 820-480q0-60.662-21-116.831Q778-653 740-699L261-220q45 39 101.493 59.5Q418.987-140 480-140ZM221-261l478-478q-46-39-102.169-60T480-820q-142.375 0-241.188 98.812Q140-622.375 140-480q0 61.013 22 117.507Q184-306 221-261Z" /></svg>
+                                </div>
+                                <div className="delete-profile-icon">
+                                    <svg lassName="delete-profile-icon " xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 -960 960 960" width="48"><path d="m249-207-42-42 231-231-231-231 42-42 231 231 231-231 42 42-231 231 231 231-42 42-231-231-231 231Z" /></svg>
+                                </div>
+                            </div>
+
                         </div>
                     </div>
                 </div>
 
             </div>
 
-            <div className="d-flex delete-profile me-4">
-                <div className="block-icon-profile me-3">
-                    <svg lassName="block-icon-profile" xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 -960 960 960" width="48"><path d="M480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-60q142.375 0 241.188-98.812Q820-337.625 820-480q0-60.662-21-116.831Q778-653 740-699L261-220q45 39 101.493 59.5Q418.987-140 480-140ZM221-261l478-478q-46-39-102.169-60T480-820q-142.375 0-241.188 98.812Q140-622.375 140-480q0 61.013 22 117.507Q184-306 221-261Z" /></svg>
-                </div>
-                <div className="delete-profile-icon">
-                    <svg lassName="delete-profile-icon " xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 -960 960 960" width="48"><path d="m249-207-42-42 231-231-231-231 42-42 231 231 231-231 42 42-231 231 231 231-42 42-231-231-231 231Z" /></svg>
-                </div>
-            </div>
 
         </div >
     )
