0e818dd17463a9880e146f3381314137824d2e54
Samessprog <100785560+Samessprog@users.noreply.github.com>
2023-09-09 22:47:32 +0200

    DrinksAdmin fetching and loading

--- APP/src/JS/Profile/admin/DrinksProfile.js
+++ APP/src/JS/Profile/admin/DrinksProfile.js
@@ -9,23 +9,48 @@ function DrinksProfile({ elm }) {
     const [drinkProfileIMG, setdrinkProfileIMG] = useState('')
 
 
-    //ADD Fetch IMG from DB
+
+    const [drinkIMGs, setDrinkIMG] = useState(null);
+    const [convertetIMG, setConvertedIMG] = useState('')
     const [fetchIMGCompleted, setFetchIMGCompleted] = useState(false)
-    console.log(elm)
 
 
 
     useEffect(() => {
-        if (elm.IMG && elm.IMG.data) {
+        const fetchUserFavouriteDrinkImage = async () => {
+            try {
+                let ID_Drink = elm.ID_DRINK;
+                const response = await fetch(`http://localhost:3000/api/fetchDrinkIMG/${ID_Drink}`, {
+                    credentials: 'include',
+                });
+                if (!response.ok) {
+                    throw new Error('Failed to fetch user favorite drink image.');
+                }
+                // Parsuj odpowiedź jako JSON
+                const data = await response.json();
+                setDrinkIMG(data.image);
+
+            } catch (error) {
+                console.error(error);
+            }
+        };
+        fetchUserFavouriteDrinkImage();
+    }, [elm.ID_DRINK]);
+
+
+    useEffect(() => {
+        if (drinkIMGs && drinkIMGs.data.length > 0) {
             // Convert the image data to base64
-            const base64Image = Buffer.from(elm.IMG.data).toString('base64');
+            const base64Image = Buffer.from(drinkIMGs.data).toString('base64');
             // Create the image URL using the base64 data
             const imageURL = `data:image/jpeg;base64,${base64Image}`;
-            setdrinkProfileIMG(imageURL);
+            setConvertedIMG(imageURL);
+            setFetchIMGCompleted(true)
+        } else {
+            setConvertedIMG('https://staticsmaker.iplsc.com/smaker_production_2021_11_24/d9d5fac2c9271afdbc7205b695742eca-lg.jpg');
         }
 
-    }, []);
-
+    }, [drinkIMGs]);
 
     return (
 
@@ -39,15 +64,17 @@ function DrinksProfile({ elm }) {
                         <div className="d-flex align-items-center data-holder ">
                             <div className=" mt-1 mb-1 drink-profile-holder-IMG">
                                 {fetchIMGCompleted ? (
-                                    <img className=" drink-profile-img img-fluid " src={drinkProfileIMG} alt="loadingErr"></img>
+                                    <img className=" drink-profile-img img-fluid " src={convertetIMG} alt="loadingErr"></img>
 
                                 ) : (
-                                    <Ring
-                                        size={155}
-                                        lineWeight={5}
-                                        speed={2}
-                                        color="black"
-                                    />
+                                    <div className="ps-4 pe-4 pb-4 pt-4">
+                                        <Ring
+                                            size={155}
+                                            lineWeight={3}
+                                            speed={2}
+                                            color="black"
+                                        />
+                                    </div>
                                 )}
                             </div>
                             <div className="ms-4 drink-name-profile">
