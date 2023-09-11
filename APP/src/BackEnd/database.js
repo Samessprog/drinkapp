e6508372a2e0d1f7898998b282828b2a865d9dee
Samessprog <100785560+Samessprog@users.noreply.github.com>
2023-09-11 21:56:00 +0200

    User Delete form Admin panel

--- APP/src/BackEnd/database.js
+++ APP/src/BackEnd/database.js
@@ -70,6 +70,31 @@ app.post('/api/removeFromUserFavourite', async (req, res) => {
   });
 });
 
+app.post('/api/deleteUser', async (req, res) => {
+  const { userID } = req.body;
+
+  if (!userID) {
+    res.status(400).json({ error: 'userID is required' });
+    return;
+  }
+
+  db.query('DELETE FROM users WHERE ID_User = ?', userID, (err, results) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Internal Server Error' });
+      return;
+    }
+
+    if (results.affectedRows > 0) {
+      res.status(200).json({ message: 'User deleted successfully' });
+    } else {
+      res.status(404).json({ error: 'User not found' });
+    }
+  });
+});
+
+
+
 //Gets 
 app.get('/api/takeFavouriteUserDrink', async (req, res) => {
   const userIDs = req.session.user?.userID;
--- APP/src/JS/Profile/admin/UsersAdminControlerProfile.js
+++ APP/src/JS/Profile/admin/UsersAdminControlerProfile.js
@@ -84,7 +84,32 @@ function UsersAdminControlerProfile({ elm }) {
         }
     };
 
-    
+
+    const deleteUser = async () => {
+        const userID = elm.ID_User;
+        event.preventDefault();
+        try {
+            const response = await fetch(`${API_URL}deleteUser`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ userID }),
+            });
+
+            const data = await response.json();
+
+            if (response.status === 200 && data.message === 'User deleted successfully') {
+                alert('Success');
+            } else if (response.status === 404 && data.error === 'User not found') {
+                alert('User not found');
+            }
+        } catch (error) {
+            console.error(error);
+            alert('An error occurred');
+        }
+    };
+
     return (
         <div className="mb-3 ms-3 d-flex align-items-center drinks-profile-holder  me-3  justify-content-between ">
             <div className="d-flex align-items-center ">
@@ -154,7 +179,7 @@ function UsersAdminControlerProfile({ elm }) {
                                 <div className="block-icon-profile me-3">
                                     <svg lassName="block-icon-profile" xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 -960 960 960" width="48"><path d="M480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-60q142.375 0 241.188-98.812Q820-337.625 820-480q0-60.662-21-116.831Q778-653 740-699L261-220q45 39 101.493 59.5Q418.987-140 480-140ZM221-261l478-478q-46-39-102.169-60T480-820q-142.375 0-241.188 98.812Q140-622.375 140-480q0 61.013 22 117.507Q184-306 221-261Z" /></svg>
                                 </div>
-                                <div className="delete-profile-icon">
+                                <div onClick={deleteUser} className="delete-profile-icon">
                                     <svg lassName="delete-profile-icon " xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 -960 960 960" width="48"><path d="m249-207-42-42 231-231-231-231 42-42 231 231 231-231 42 42-231 231 231 231-42 42-231-231-231 231Z" /></svg>
                                 </div>
                             </div>
