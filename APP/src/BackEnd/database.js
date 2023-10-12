8b87b7f8113b10c8deb7f7bf7eb3f55318f4137d
Samessprog <100785560+Samessprog@users.noreply.github.com>
2023-10-12 17:20:07 +0200

    DrinksRating ;o

--- APP/src/BackEnd/database.js
+++ APP/src/BackEnd/database.js
@@ -250,18 +250,85 @@ app.post('/api/acceptDrinksByAdmin', async (req, res) => {
 app.post('/api/drinkRating', async (req, res) => {
   const { drinkID, clickedStar, userID } = req.body;
 
-  const query = "INSERT INTO drinksrating (User_ID, Drink_ID, Rate) VALUES (?, ?, ?)";
-
-  connectionToDrinksDB.query(query, [userID, drinkID, clickedStar], (err, result) => {
-    if (err) {
-      console.error("Błąd przy wstawianiu danych do tabeli DrinkRating:", err);
-      res.status(500).send("Wystąpił błąd podczas oceniania drinku.");
+  // Check if the user has already rated this drink
+  const checkQuery = "SELECT * FROM drinksrating WHERE User_ID = ? AND Drink_ID = ?";
+  connectionToDrinksDB.query(checkQuery, [userID, drinkID], (checkErr, checkResult) => {
+    if (checkErr) {
+      console.error("Błąd przy sprawdzaniu oceny drinku:", checkErr);
+      res.status(500).send("Wystąpił błąd podczas sprawdzania oceny drinku.");
     } else {
-      console.log("Dane oceny drinku zostały pomyślnie wstawione do tabeli DrinkRating.");
-      res.status(200).send("Ocena drinku została dodana.");
+      if (checkResult.length > 0) {
+        // The user has already rated this drink, so update the existing rating
+        const updateQuery = "UPDATE drinksrating SET Rate = ? WHERE User_ID = ? AND Drink_ID = ?";
+        connectionToDrinksDB.query(updateQuery, [clickedStar, userID, drinkID], (updateErr, updateResult) => {
+          if (updateErr) {
+            console.error("Błąd przy aktualizowaniu oceny drinku:", updateErr);
+            res.status(500).send("Wystąpił błąd podczas aktualizacji oceny drinku.");
+          } else {
+            console.log("Ocena drinku została zaktualizowana.");
+
+            // Calculate the average rating for the drink
+            const averageQuery = "SELECT AVG(Rate) AS AverageRating FROM drinksrating WHERE Drink_ID = ?";
+            connectionToDrinksDB.query(averageQuery, [drinkID], (averageErr, averageResult) => {
+              if (averageErr) {
+                console.error("Błąd przy obliczaniu średniej oceny drinku:", averageErr);
+              } else {
+                const averageRating = averageResult[0].AverageRating;
+                console.log(`Średnia ocena dla drinku (Drink ID: ${drinkID}): ${averageRating}`);
+
+                // Update the "drink" table with the calculated average rating
+                const updateDrinkTableQuery = "UPDATE drink SET Rate = ? WHERE ID_Drink = ?";
+                connectionToDrinksDB.query(updateDrinkTableQuery, [averageRating, drinkID], (updateDrinkErr, updateDrinkResult) => {
+                  if (updateDrinkErr) {
+                    console.error("Błąd przy aktualizacji tabeli drink:", updateDrinkErr);
+                  } else {
+                    console.log(`Tabela drink została zaktualizowana z nową oceną (Drink ID: ${drinkID}): ${averageRating}`);
+                  }
+                });
+              }
+            });
+
+            res.status(200).send("Ocena drinku została zaktualizowana.");
+          }
+        });
+      } else {
+        // The user has not rated this drink before, insert a new rating
+        const insertQuery = "INSERT INTO drinksrating (User_ID, Drink_ID, Rate) VALUES (?, ?, ?)";
+        connectionToDrinksDB.query(insertQuery, [userID, drinkID, clickedStar], (insertErr, insertResult) => {
+          if (insertErr) {
+            console.error("Błąd przy wstawianiu oceny drinku:", insertErr);
+            res.status(500).send("Wystąpił błąd podczas oceniania drinku.");
+          } else {
+            console.log("Ocena drinku została pomyślnie dodana.");
+
+            // Calculate the average rating for the drink
+            const averageQuery = "SELECT AVG(Rate) AS AverageRating FROM drinksrating WHERE Drink_ID = ?";
+            connectionToDrinksDB.query(averageQuery, [drinkID], (averageErr, averageResult) => {
+              if (averageErr) {
+                console.error("Błąd przy obliczaniu średniej oceny drinku:", averageErr);
+              } else {
+                const averageRating = averageResult[0].AverageRating;
+                console.log(`Średnia ocena dla drinku (Drink ID: ${drinkID}): ${averageRating}`);
+
+                // Update the "drink" table with the calculated average rating
+                const updateDrinkTableQuery = "UPDATE drink SET Rate = ? WHERE ID_Drink = ?";
+                connectionToDrinksDB.query(updateDrinkTableQuery, [averageRating, drinkID], (updateDrinkErr, updateDrinkResult) => {
+                  if (updateDrinkErr) {
+                    console.error("Błąd przy aktualizacji tabeli drink:", updateDrinkErr);
+                  } else {
+                    console.log(`Tabela drink została zaktualizowana z nową oceną (Drink ID: ${drinkID}): ${averageRating}`);
+                  }
+                });
+              }
+            });
+            res.status(200).send("Ocena drinku została dodana.");
+          }
+        });
+      }
     }
   });
 });
+
 
 const multer = require('multer');
 const upload = multer();
