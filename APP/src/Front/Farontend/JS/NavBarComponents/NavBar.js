8530cbd56a70265e3374344e1fb282db70469a73
Samessprog <100785560+Samessprog@users.noreply.github.com>
2023-11-28 19:55:19 +0100

    Anime fix 2

--- APP/src/Front/Farontend/JS/NavBarComponents/NavBar.js
+++ APP/src/Front/Farontend/JS/NavBarComponents/NavBar.js
@@ -120,7 +120,9 @@ function NavBar({ setSearchingDrink, searchingDrink, drinkDatas,
                 <div className={`col-12 d-flex align-items-center ${userScroll ? 'pb-3' : 'pb-0'}`} >
                     <div className={userScroll ? `d-none` : `col-xl-4 brand-name-SCROLL d-md-flex ${location.pathname.endsWith('/') ? '' : 'col-xl-10 col-xxl-4'}`}>
                         <Link className="d-flex align-items-center logo-link" to="/">
-                            <label className="brand-name-nav">𝒞𝑜𝒸𝓀𝓉𝒶𝒾𝓁 𝒫𝒶𝓇𝓉𝓎</label>
+                            <label className="brand-name-nav">
+                                𝒞𝑜𝒸𝓀𝓉𝒶𝒾𝓁 𝒫𝒶𝓇𝓉𝓎
+                            </label>
                             <svg className="ms-3 brand-logo d-none d-xxl-block" xmlns="http://www.w3.org/2000/svg" height="1.1em" viewBox="0 0 576 512"><path d="M432 240c53 0 96-43 96-96s-43-96-96-96c-35.5 0-66.6 19.3-83.2 48H296.2C316 40.1 369.3 0 432 0c79.5 0 144 64.5 144 144s-64.5 144-144 144c-27.7 0-53.5-7.8-75.5-21.3l35.4-35.4c12.2 5.6 25.8 8.7 40.1 8.7zM1.8 142.8C5.5 133.8 14.3 128 24 128H392c9.7 0 18.5 5.8 22.2 14.8s1.7 19.3-5.2 26.2l-177 177V464h64c13.3 0 24 10.7 24 24s-10.7 24-24 24H208 120c-13.3 0-24-10.7-24-24s10.7-24 24-24h64V345.9L7 169c-6.9-6.9-8.9-17.2-5.2-26.2z" /></svg>
                         </Link>
                     </div>
@@ -248,7 +250,7 @@ function NavBar({ setSearchingDrink, searchingDrink, drinkDatas,
                 <div className={userScroll && window.innerWidth > 768 ? 'main-options-holder d-none' : 'main-options-holder d-flex'} >
                     <div className="Options-Holder-W100 ">
                         <ul className="d-flex justify-content-center mt-1 navbar-menu ps-0">
-                            <li className=" ms-2  elm-contents-holder " >
+                            <li className=" ms-2  elm-contents-holder" >
                                 <Link className="elm-contents text-decoration-none" to="/">Home</Link>
                             </li>
 
